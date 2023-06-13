86bf775d665fc420a6a760bd5b8d95ee8699c110
vivek06298 <vivek06298@gmail.com>
2023-06-13 20:44:20 +0530

    Section 1 HomePage

--- components/core/Homepage/CodeBlocks.jsx
+++ components/core/Homepage/CodeBlocks.jsx
@@ -11,25 +11,23 @@ const CodeBlocks = ({
         
         {/*Section 1*/}
         <div className='w-[50%] flex flex-col gap-8'>
-
             {heading}
             <div className='text-richblack-300 font-bold '>
                 {subheading}
             </div>
 
-        </div>
+            <div className='flex gap-7 mt-7'>
+                <CTAButton active={ctabtn1.active} linkto={ctabtn1.linkto}>
+                    <div className='flex gap-2 items-center'>
+                        {ctabtn1.btnText}
+                        <FaArrowRight/>
+                    </div>
+                </CTAButton>
 
-        <div className='flex gap-7 mt-7'>
-            <CTAButton active={ctabtn1.active} linkto={ctabtn1.linkto}>
-                <div className='flex gap-2 items-center'>
-                    {ctabtn1.btnText}
-                    <FaArrowRight/>
-                </div>
-            </CTAButton>
-
-            <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>  
-                    {ctabtn2.btnText}
-            </CTAButton>
+                <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>  
+                        {ctabtn2.btnText}
+                </CTAButton>
+            </div>
         </div>
 
         {/*Section 2*/}
